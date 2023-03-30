a3ae72c9bc3ff070ad31f992aa6f943526981199
george-polygod <george@polygod.io>
2023-03-31 00:27:00 +0300

    files/

--- send.php
+++ send.php
@@ -169,7 +169,7 @@ if (!$errors) {
 	echo '<div style="color: #e9431c">' . $errors . '</div>';
 	echo '<div id="success">';    
 	echo '<h4>Something went wrong.</h4>';
-	echo '<a class="animated-link" href="../index.html">Go Back</small>';
+	echo '<a class="animated-link" href="../">Go Back</small>';
 	echo '</div>';	
 }
 
