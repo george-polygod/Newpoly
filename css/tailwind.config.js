7bbd59dc2b4406ea5c0df9011190836b831027eb
MasterCryptoLord <106593993+MasterCryptoLord@users.noreply.github.com>
2023-04-12 15:24:35 +0300

    gametail_update_10

---
+++ css/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  theme: {
+    extend: {
+      spacing: {
+        '200': '50rem', // or any other value you prefer
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
